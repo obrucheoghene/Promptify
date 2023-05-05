1295166a0ef8b14677a2898782206f1b8a5589f7
wilfredcloud <wilfredcloudspace@gmail.com>
2023-05-05 21:58:17 +0100

    feat: Add Navbar

---
+++ components/Nav.tsx
@@ -0,0 +1,114 @@
+'use client';
+import { APP_NAME } from '@utils/constant';
+import Image from 'next/image';
+import Link from 'next/link';
+import React, { useEffect, useState } from 'react';
+import {
+  signIn,
+  signOut,
+  getProviders,
+  useSession,
+  LiteralUnion,
+  ClientSafeProvider,
+} from 'next-auth/react';
+import { BuiltInProviderType } from 'next-auth/providers';
+
+const Nav = () => {
+  const isUserLoggedIn = true;
+  const [providers, setProviders] = useState<Record<
+    LiteralUnion<BuiltInProviderType, string>,
+    ClientSafeProvider
+  > | null>(null);
+
+  useEffect(() => {
+    const fetchProviders = async () => {
+      const response = await getProviders();
+      setProviders(response);
+    };
+    fetchProviders();
+  }, []);
+  return (
+    <nav className=" flex-between w-full mb-16 pt-3">
+      <Link href="/" className=" flex gap-2 flex-center">
+        <Image
+          src="/assets/images/logo.svg"
+          alt={`${APP_NAME} logo`}
+          width={30}
+          height={30}
+        />
+        <p className=" logo_text">{APP_NAME}</p>
+      </Link>
+
+      {/* Desktop navigation */}
+      <div className=" sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              Create Post
+            </Link>
+            <button
+              type="button"
+              onClick={() => signOut()}
+              className="outline_btn"
+            >
+              Sign Out
+            </button>
+            <Link href="/profile">
+              <Image
+                src="/assets/images/logo.svg"
+                width={37}
+                height={37}
+                alt="Profile"
+              />
+            </Link>
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className="black_btn"
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
+
+      {/* Mobile Navigation */}
+      <div className="sm:hidden flex relative">
+        {isUserLoggedIn ? (
+          <div className="flex">
+            <Image
+              src="/assets/images/logo.svg"
+              width={37}
+              height={37}
+              alt="Profile"
+              onClick={() => {}}
+            />
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className="black_btn"
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
