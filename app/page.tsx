78ece3c270dacf20d0a817555f6d615a6c99705d
wilfredcloud <wilfredcloudspace@gmail.com>
2023-05-06 05:13:47 +0100

    refactor: Use APP_Name instead of hardcoding

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Feed from '@components/Feed';
+import { APP_NAME } from '@utils/constant';
 
 const Home = () => {
   return (
@@ -9,7 +10,7 @@ const Home = () => {
         <span className="orange_gradient text-center">AI-Powered Prompts</span>
       </h1>
       <p className=" desc text-center">
-        Promptopia is an open-source AI prompting tool for modern world to
+        {APP_NAME} is an open-source AI prompting tool for modern world to
         discover, create and share creative prompts
       </p>
       <Feed />
