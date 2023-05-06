1661b7370c11e2be8a2e93101c502236320a2a51
wilfredcloud <wilfredcloudspace@gmail.com>
2023-05-06 20:28:32 +0100

    feat(api): Add get prompt api

---
+++ app/api/prompt/route.ts
@@ -0,0 +1,16 @@
+import { connectToDB } from '@utils/database';
+import { Prompt } from '@models/prompt';
+import { NextResponse } from 'next/server';
+
+export const GET = async (req: Request) => {
+  try {
+    await connectToDB();
+    const prompts = await Prompt.find({}).populate('creator');
+    await connectToDB();
+
+    return new NextResponse(JSON.stringify(prompts), { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ error: 'error occured' }, { status: 500 });
+  }
+};
