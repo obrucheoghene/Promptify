14f1850e021db4ef86ce041141335b975b042c34
wilfredcloud <wilfredcloudspace@gmail.com>
2023-05-06 05:44:04 +0100

    fix: Save authenticaticated user to db

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -11,33 +11,35 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
-  async session({ session }: { session: Record<string, any> }) {
-    const sessionUser = await User.findOne({
-      email: session.user.email,
-    });
-    session.user.id = sessionUser._id.toString();
-    return session;
-  },
-  async signIn({ profile }: { profile: Record<string, any> }) {
-    try {
-      await connectToDB();
-      // check if user already exists
-      const userExists = await User.findOne({ email: profile.email });
+  callbacks: {
+    async session({ session }: { session: Record<string, any> }) {
+      const sessionUser = await User.findOne({
+        email: session.user.email,
+      });
+      session.user.id = sessionUser._id.toString();
+      return session;
+    },
+    async signIn({ profile }: { profile: Record<string, any> }) {
+      try {
+        await connectToDB();
+        // check if user already exists
+        const userExists = await User.findOne({ email: profile.email });
 
-      if (!userExists) {
-        await User.create({
-          email: profile.email,
-          username: profile.name.replace(' ', '').toLowerCase(),
-          image: profile.picture,
-        });
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(' ', '').toLowerCase(),
+            image: profile.picture,
+          });
+        }
+
+        // if not create a new user
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
       }
-
-      // if not create a new user
-      return true;
-    } catch (error) {
-      console.log(error);
-      return false;
-    }
+    },
   },
 });
 
