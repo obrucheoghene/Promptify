6d74955a6e74d887abb031d444f5fcd94b4146d7
wilfredcloud <wilfredcloudspace@gmail.com>
2023-05-06 05:14:09 +0100

    feat(api): Create nexthauth route

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,44 @@
+import NextAuth from 'next-auth/next';
+import GoogleProvider from 'next-auth/providers/google';
+import { signIn } from 'next-auth/react';
+import { connectToDB } from '@utils/database';
+import User from '@models/user';
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  async session({ session }: { session: Record<string, any> }) {
+    const sessionUser = await User.findOne({
+      email: session.user.email,
+    });
+    session.user.id = sessionUser._id.toString();
+    return session;
+  },
+  async signIn({ profile }: { profile: Record<string, any> }) {
+    try {
+      await connectToDB();
+      // check if user already exists
+      const userExists = await User.findOne({ email: profile.email });
+
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(' ', '').toLowerCase(),
+          image: profile.picture,
+        });
+      }
+
+      // if not create a new user
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  },
+});
+
+export { handler as GET, handler as POST };
