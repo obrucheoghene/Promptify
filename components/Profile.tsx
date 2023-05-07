b70b12d0656d33325d022d7c1f4c40a89d6d68b9
wilfredcloud <wilfredcloudspace@gmail.com>
2023-05-07 14:51:17 +0100

    feat: Add Profile page

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,7 +1,40 @@
 import React from 'react';
+import PromptCard from './PromptCard';
 
-const Profile = () => {
-  return <div>Profile</div>;
+interface ProfileProps {
+  name: string;
+  desc: string;
+  data: Record<string, any>[];
+  handleEdit: (post: Record<string, any>) => void;
+  handleDelete: (post: Record<string, any>) => void;
+}
+
+const Profile = ({
+  name,
+  desc,
+  data,
+  handleDelete,
+  handleEdit,
+}: ProfileProps) => {
+  return (
+    <section className="w-full">
+      <h1 className="head_text text-left">
+        <span className="blue_gradient">{name} Profile</span>
+      </h1>
+      <p className="desc text-left">{desc}</p>
+
+      <div className="mt-10 prompt_layout">
+        {data.map((post) => (
+          <PromptCard
+            key={post._id}
+            post={post}
+            handleEdit={() => handleEdit && handleEdit(post)}
+            handleDelete={() => handleDelete && handleDelete(post)}
+          />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Profile;
