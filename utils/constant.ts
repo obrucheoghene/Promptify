804aa157297d5a32a5848e52aab31b7ead0e2dd5
wilfredcloud <wilfredcloudspace@gmail.com>
2023-05-08 15:12:42 +0100

    feat: Add post message as const value

--- utils/constant.ts
+++ utils/constant.ts
@@ -1,2 +1,3 @@
 export const APP_NAME = 'Promptify';
 export const WELCOME_MESSAGE = `Welcome to ${APP_NAME} - the ultimate open-source AI prompting platform for saving and sharing creative prompts!`;
+export const POST_MESSAGE = `Unleash your creativity and share your most amazing prompts with the world using ${APP_NAME}`;
