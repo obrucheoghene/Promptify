095e9a12fd898f7d828c985ebffbba750ff01152
wilfredcloud <wilfredcloudspace@gmail.com>
2023-06-25 16:08:05 +0100

    feat: Augmented Session user in nextauth

---
+++ next-auth.d.ts
@@ -0,0 +1,13 @@
+import NextAuth, { DefaultSession } from "next-auth"
+
+declare module "next-auth" {
+  /**
+   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface User {
+    id: String
+  }
+  interface Session extends DefaultSession {
+    user?: User
+  }
+}
